92ed54ef3c45ac9e32cda92c995e76692357eecd
ycchien7551 <40550120+ycchien7551@users.noreply.github.com>
2018-06-25 08:18:24 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,21 +1,17 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
 namespace tools {
-    /**
-    * 計算長方形面積，並回傳
-    */
     //% blockId="trunLeft" block="trunLeft"
-    //% blockGap=1 weight=0 blockExternalInputs=true
+    //% blockGap=0 weight=0 blockExternalInputs=true
     export function trunLeft () {
         if (input.acceleration(Dimension.X) < -100) {
           return ture;
         }
     }
-    /**
-    * 計算長方形面積，不回傳，只顯示在LED
-    */
-    //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
-    //% blockGap=2 weight=1
-    export function ledOfRectangle(length: number, width:number): void {
-        basic.showNumber(length*width)
+    //% blockId="trunRight" block="trunRight"
+    //% blockGap=0 weight=0
+    export function trunRight () {
+        if (input.acceleration(Dimension.X) > 100) {
+          return ture;
+        }
     }
 }
